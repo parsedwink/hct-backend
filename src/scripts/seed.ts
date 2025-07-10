77eabee662025f4aa99d716566332287ebbbaff3
Leo Nistor <leo.nistor@gmail.com>
2025-07-10 22:42:21 +0300

    seed prods and imgprods

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -62,6 +62,7 @@ export const script = async (config: SanitizedConfig) => {
       data: { alt: `Logo ${name}` },
       filePath: imageFilePath,
     })
+    // partener
     const created = await payload.create({
       collection: 'parteneri',
       data: {
@@ -72,6 +73,7 @@ export const script = async (config: SanitizedConfig) => {
         url: url,
       },
     })
+    // lookup for produs
     created_partneri.set(id, created)
     payload.logger.info(`created partner: ${id}`)
   })
@@ -79,46 +81,74 @@ export const script = async (config: SanitizedConfig) => {
   payload.logger.info('done: Parteneri')
 
   // produse
+  // imgprod_placeholder
+  await payload.create({
+    collection: 'imgprod',
+    filePath: PLACEHOLDER,
+    data: { alt: 'Image Placeholder' },
+  })
   result = imp_prodse.map(async (p) => {
     const { name, code, url, imp_categ, img_name, desc } = p
+
     // imagine produs
-    let imageFilePath
+    let imageOK = true
+    const imageFilePath = `${BASE}imagini_produse/${img_name}`
+
+    // check if image exists
     if (img_name.length > 0) {
-      imageFilePath = `${BASE}imagini_produse/${img_name}`
       if (!existsSync(imageFilePath)) {
-        imageFilePath = PLACEHOLDER
+        imageOK = false
         payload.logger.warn(`code ${code}, name ${name}, image not found: ${img_name}`)
       }
     } else {
-      imageFilePath = PLACEHOLDER
+      imageOK = false
       payload.logger.warn(`code ${code}, name ${name}, has no image`)
     }
-    const { id: uploadedImage } = await payload.create({
-      collection: 'imgprod',
-      data: { alt: `${code} ${name}` },
-      filePath: imageFilePath,
-    })
+
     // get partener from code
     const partener = created_partneri.get(code)
-    // create produs
-    await payload.create({
-      collection: 'produse',
-      data: {
-        nume: name,
-        descriere: desc,
-        partener: partener,
-        imagini: [
-          {
-            relationTo: 'imgprod',
-            value: uploadedImage,
-          },
-        ],
-        url_producator: url,
-        import_img_name: img_name,
-        import_cod_partener: code,
-        import_categorie: imp_categ,
-      },
-    })
+
+    if (imageOK) {
+      // create produs with image
+      const { id: uploadedImage } = await payload.create({
+        collection: 'imgprod',
+        data: { alt: `${code} ${name}` },
+        filePath: imageFilePath,
+      })
+      await payload.create({
+        collection: 'produse',
+        data: {
+          nume: name,
+          descriere: desc,
+          partener: partener,
+          imagini: [
+            {
+              relationTo: 'imgprod',
+              value: uploadedImage,
+            },
+          ],
+          url_producator: url,
+          import_img_name: img_name,
+          import_cod_partener: code,
+          import_categorie: imp_categ,
+        },
+      })
+    } else {
+      // create produs without image
+      await payload.create({
+        collection: 'produse',
+        data: {
+          nume: name,
+          descriere: desc,
+          partener: partener,
+          imagini: [],
+          url_producator: url,
+          import_img_name: img_name,
+          import_cod_partener: code,
+          import_categorie: imp_categ,
+        },
+      })
+    }
     payload.logger.info(`created produs: ${name}`)
   })
   await Promise.all(result)
