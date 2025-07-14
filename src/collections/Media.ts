18d738d46f503da465597b3f1d36ec1605e8df8c
Leo Nistor <leo.nistor@gmail.com>
2025-07-14 08:00:54 +0300

    add media collection image sizes

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -14,5 +14,40 @@ export const Media: CollectionConfig = {
   ],
   upload: {
     staticDir: 'public/media',
+    adminThumbnail: 'thumbnail',
+    displayPreview: true,
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 300,
+      },
+      {
+        name: 'square',
+        width: 500,
+        height: 500,
+      },
+      {
+        name: 'small',
+        width: 600,
+      },
+      {
+        name: 'medium',
+        width: 900,
+      },
+      {
+        name: 'large',
+        width: 1400,
+      },
+      {
+        name: 'xlarge',
+        width: 1920,
+      },
+      {
+        name: 'og',
+        width: 1200,
+        height: 630,
+        crop: 'center',
+      },
+    ],
   },
 }
