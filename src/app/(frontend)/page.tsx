6157f1cca91aaa74a7711d7d2efb6cbebad9064d
Leo Nistor <leo.nistor@gmail.com>
2025-07-14 07:22:53 +0300

    add logo

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,12 +3,16 @@ import { getPayload } from 'payload'
 import config from '@/payload.config'
 import './styles.css'
 
+import Image from 'next/image'
+import logo from 'src/assets/hct-logo.svg'
+
 export default async function HomePage() {
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
 
   return (
     <div className="">
+      <Image src={logo} alt="HCT Logo" />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <p>Home</p>
     </div>
